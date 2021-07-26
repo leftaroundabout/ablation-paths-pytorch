06de37bd3901f1a97e3ee77c4439f03277fb4684
Justus Sagemüller <jsag@hvl.no>
2021-07-26 18:06:16 +0200

    Supporting using gradient in sobolev space for path-optim update.

--- experiments/image_filtering.py
+++ experiments/image_filtering.py
@@ -27,6 +27,8 @@ import scipy.ndimage
 
 import torch.fft
 
+import odl
+
 from enum import Enum
 
 def img_fft(image):
@@ -52,6 +54,34 @@ def apply_gaussian_filter(image, sigma):
     filtered = [sp.ndimage.filters.gaussian_filter(monochromatic, sigma=sigma)
                         for monochromatic in image.cpu()]
     return torch.Tensor(filtered).to(image.device)
+
+def get_sobolev_metric(space, scale=1.):
+    lap = odl.discr.diff_ops.Laplacian(space, pad_mode='order0')
+    op = odl.operator.IdentityOperator(space) - scale**2 * lap
+    return op
+
+def apply_sobolevdualproj_filter(image, scale):
+    if type(image) is odl.DiscretizedSpaceElement:
+        space = image.space
+        φreg = space.one()
+        odl.solvers.iterative.conjugate_gradient(
+              get_sobolev_metric(space, scale), φreg, image, niter=40)
+        return φreg.copy()
+    elif type(image) is np.ndarray:
+        d = len(image.shape)
+        space = odl.discr.uniform_discr(
+            min_pt=list(0 for _ in range(d))
+          , max_pt=list(1 for _ in range(d))
+          , shape=image.shape
+          , dtype='float32' )
+        image_odl = space.element(image)
+        return apply_sobolevdualproj_filter(image_odl, scale).asarray()
+    elif type(image) is torch.Tensor:
+        return torch.Tensor(
+                  apply_sobolevdualproj_filter(image.cpu().numpy(), scale)
+                ).to(image.device)
+    else:
+        raise TypeError("Supports only odl.DiscretisedSpaceElement, numpy.ndarray and torch.Tensor")
 
 class FilterType(Enum):
     Gaussian=0
